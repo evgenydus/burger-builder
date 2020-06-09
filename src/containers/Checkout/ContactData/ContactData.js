542f3fb4e4dff80c50183ee990a9a91f35f1addc
evgenydus <evgeny21dus@gmail.com>
2020-06-09 14:09:56 +0300

    feat: add form submition

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -68,9 +68,16 @@ class ContactData extends Component {
     event.preventDefault()
 
     this.setState({ isLoading: true })
+
+    const formData = {}
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
+
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      orderData: formData,
     }
 
     axios.post('/orders.json', order)
@@ -104,7 +111,7 @@ class ContactData extends Component {
     }
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
