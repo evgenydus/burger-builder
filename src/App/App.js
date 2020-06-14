cc1e754104c8a6378e54ea6ae9b13a606d16b4e7
evgenydus <evgeny21dus@gmail.com>
2020-06-14 17:38:27 +0300

    feat: add auth form

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import './App.css';
 
+import Auth from '../containers/Auth/Auth';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 import Checkout from '../containers/Checkout/Checkout';
 import Layout from '../components/hoc/Layout/Layout';
@@ -15,6 +16,7 @@ const App = () => {
         <Switch>
           <Route path="/checkout" component={Checkout}/>
           <Route path="/orders" component={Orders}/>
+          <Route path="/auth" component={Auth}/>
           <Route path="/" exact component={BurgerBuilder}/>
         </Switch>
       </Layout>
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react'
+
+import './Auth.css'
+
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+
+class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Email'
+        },
+        value: '',
+        validation: {
+          isRequired: true,
+          isEmail: true,
+        },
+        isValid: false,
+        isTouched: false,
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Password'
+        },
+        value: '',
+        validation: {
+          isRequired: true,
+          minLength: 6,
+        },
+        isValid: false,
+        isTouched: false,
+      },
+    }
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true
+
+    if (rules.isRequired) {
+      isValid = value.trim() !== '' && isValid
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
+    }
+
+    return isValid
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const { controls } = this.state
+    const updatedControls = {
+      ...controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        isValid: this.checkValidity(event.target.value, controls[controlName].validation),
+        isTouched: true,
+      },
+    }
+
+    this.setState({ controls: updatedControls})
+  }
+
+  render () {
+    const { controls } = this.state
+    const formElementsArray = []
+
+    for (let key in controls) {
+      formElementsArray.push({
+        id: key,
+        config: controls[key],
+      })
+    }
+
+    const form = formElementsArray.map(formElement => {
+      return (<Input
+        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+        elementConfig={formElement.config.elementConfig}
+        elementType={formElement.config.elementType}
+        invalid={!formElement.config.isValid}
+        isTouched={formElement.config.isTouched}
+        key={formElement.id}
+        shouldValidate={formElement.config.validation}
+        value={formElement.config.value}
+        />)
+    })
+
+    return (
+      <div className="auth">
+        <form>
+          {form}
+          <Button btnType={['success']}>Submit</Button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Auth
